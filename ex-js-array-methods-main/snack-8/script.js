e02572584a43dbc0f1226e3509801698aed66491
GalloSimone <gallosimonecz@gmail.com>
2024-11-05 16:01:50 +0100

    snack7-8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,5 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+const marcoClass = (students.find(item=>item.name=="Marco Lanci")).class;
+console.log(marcoClass);
